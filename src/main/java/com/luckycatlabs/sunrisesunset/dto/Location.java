1322d1aa103f9b17adb1ffd8b2611a0afc337e08
Guillermo Mazzola <gmazzo65@gmail.com>
2018-08-19 19:59:50 -0300

    Base

---
+++ src/main/java/com/luckycatlabs/sunrisesunset/dto/Location.java
@@ -0,0 +1,26 @@
+package com.luckycatlabs.sunrisesunset.dto;
+
+import java.math.BigDecimal;
+
+public class Location {
+    private BigDecimal latitude;
+    private BigDecimal longitude;
+
+    public Location(String latitude, String longitude) {
+        this.latitude = new BigDecimal(latitude);
+        this.longitude = new BigDecimal(longitude);
+    }
+
+    public Location(double latitude, double longitude) {
+        this.latitude = new BigDecimal(latitude);
+        this.longitude = new BigDecimal(longitude);
+    }
+
+    public BigDecimal getLatitude() {
+        return this.latitude;
+    }
+
+    public BigDecimal getLongitude() {
+        return this.longitude;
+    }
+}
