faee82ce4c590b97e199af925df471256da47ada
Guillermo Mazzola <gmazzo65@gmail.com>
2025-05-01 14:16:50 +0200

    Revert "Simplified location permission request (#92)" (#103)
    * Revert "Simplified location permission request (#92)"
    
    This reverts commit 94c95281513f22141b4985f897b8d789aa2565b0.
    
    * Updated screenshots
    
    ---------
    
    Co-authored-by: GitHub Actions <github.actions@users.noreply.github.com>

--- app/src/main/kotlin/io/github/gmazzo/android/livewallpaper/weather/PermissionUtils.kt
+++ app/src/main/kotlin/io/github/gmazzo/android/livewallpaper/weather/PermissionUtils.kt
@@ -2,9 +2,11 @@
 
 package io.github.gmazzo.android.livewallpaper.weather
 
+import android.Manifest.permission.ACCESS_BACKGROUND_LOCATION
 import android.Manifest.permission.ACCESS_COARSE_LOCATION
 import android.content.Context
 import android.content.pm.PackageManager.PERMISSION_GRANTED
+import android.os.Build
 import androidx.core.content.ContextCompat
 
 private fun Context.hasPermission(permission: String) =
@@ -12,3 +14,8 @@ private fun Context.hasPermission(permission: String) =
 
 val Context.hasLocationPermission
     get() = hasPermission(ACCESS_COARSE_LOCATION)
+
+val Context.hasBackgroundLocationPermission
+    get() =
+        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.Q) hasLocationPermission
+        else hasPermission(ACCESS_BACKGROUND_LOCATION)
