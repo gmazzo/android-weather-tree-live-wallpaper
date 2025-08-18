bd2f16d463aee4faeb90004e9bbd833f8147900c
Guillermo Mazzola <gmazzo65@gmail.com>
2025-08-18 16:15:24 +0200

    Bumped to `SDK 36`

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -24,7 +24,7 @@ java.toolchain.languageVersion = JavaLanguageVersion.of(17)
 
 android {
     namespace = "io.github.gmazzo.android.livewallpaper.weather"
-    compileSdk = 35
+    compileSdk = 36
 
     defaultConfig {
         minSdk = 26
